bfc94b6aac41535e0efb5277ca1fd4d7e9e4446a
Jeff Li <lijinfeng01@ieisystem.com>
2024-08-30 23:31:05 +0800

    feat: impl tray icon
    Signed-off-by: Jeff Li <lijinfeng01@ieisystem.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
+use std::sync::mpsc;
+use tray_item::{IconSource, TrayItem};
+
+enum Message {
+    Quit,
+    Add,
+    Clear,
+    Config,
+    About,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut tray = TrayItem::new(
+        "MonitorHosts",
+        IconSource::Resource("default-icon"),
+    )
+    .unwrap();
+    tray.inner_mut().set_tooltip("MonitorHosts").unwrap();
+
+    tray.add_label("MonitorHosts").unwrap();
+
+    tray.inner_mut().add_separator().unwrap();
+
+    let (tx, rx) = mpsc::sync_channel(1);
+
+    let tx_clone = tx.clone();
+    tray.add_menu_item("添加", move || {
+        tx_clone.send(Message::Add).unwrap();
+    })
+    .unwrap();
+
+    let tx_clone = tx.clone();
+    tray.add_menu_item("清除", move || {
+        tx_clone.send(Message::Clear).unwrap();
+    })
+    .unwrap();
+
+    let tx_clone = tx.clone();
+    tray.add_menu_item("配置", move || {
+        tx_clone.send(Message::Config).unwrap();
+    })
+    .unwrap();
+
+    let tx_clone = tx.clone();
+    tray.add_menu_item("关于", move || {
+        tx_clone.send(Message::About).unwrap();
+    })
+    .unwrap();
+
+    tray.inner_mut().add_separator().unwrap();
+
+    let tx_clone = tx.clone();
+    tray.add_menu_item("Quit", move || {
+        tx_clone.send(Message::Quit).unwrap();
+    })
+    .unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(Message::Quit) => {
+                println!("Quit");
+                break;
+            }
+            Ok(Message::Add) => {
+                println!("Add");
+            }
+            Ok(Message::Clear) => {
+                println!("Clear");
+            }
+            Ok(Message::Config) => {
+                println!("Config");
+            }
+            Ok(Message::About) => {
+                println!("About");
+            }
+            _ => {}
+        }
+    }
 }
